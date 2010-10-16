1ba3928f3cfcb23bc7fef65651b00ec7fc8af946
Gabriel Filion <lelutin@gmail.com>
2010-10-16 10:19:25 -0400

    Add a little flexibility in config file sources.
    Add two sources that make it possible to use a different module to
    customize the contents of the config file. The first source gives a
    per-fqdn file and the second gives a general file.
    
    Signed-off-by: Gabriel Filion <lelutin@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,9 @@ class apt-cacher-ng {
   }
 
   file { "/etc/apt-cacher-ng/acng.conf":
-    source => "puppet:///apt-cacher-ng/acng.conf",
+    source => ["puppet:///site-apt-cacher-ng/$fqdn/acng.conf",
+               "puppet:///site-apt-cacher-ng/acng.conf",
+               "puppet:///apt-cacher-ng/acng.conf"],
     notify => Service[apt-cacher-ng],
     require => Package[apt-cacher-ng]
   }
