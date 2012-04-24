9dce78594b1ffbc8f31d4d1524b5e1bcb1263f3f
Gabriel Filion <lelutin@gmail.com>
2012-04-24 03:51:00 -0400

    Correct style for init.pp
    Signed-off-by: Gabriel Filion <lelutin@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,17 +8,17 @@ class apt-cacher-ng($version = 'installed') {
     require => Package['apt-cacher-ng'],
   }
 
-  file { "/etc/apt-cacher-ng/acng.conf":
-    source => ["puppet:///modules/site-apt-cacher-ng/$fqdn/acng.conf",
-               "puppet:///modules/site-apt-cacher-ng/acng.conf",
-               "puppet:///modules/apt-cacher-ng/acng.conf"],
+  file { '/etc/apt-cacher-ng/acng.conf':
+    source => ["puppet:///modules/site-apt-cacher-ng/${::fqdn}/acng.conf",
+               'puppet:///modules/site-apt-cacher-ng/acng.conf',
+               'puppet:///modules/apt-cacher-ng/acng.conf'],
     notify  => Service['apt-cacher-ng'],
     require => Package['apt-cacher-ng'],
   }
 
-  file { "/var/cache/apt-cacher-ng":
+  file { '/var/cache/apt-cacher-ng':
     ensure  => directory,
-    owner   => apt-cacher-ng,
+    owner   => 'apt-cacher-ng',
     require => Package['apt-cacher-ng'],
   }
 }
