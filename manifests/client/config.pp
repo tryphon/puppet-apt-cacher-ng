8825efe3c133a4407b68fa2017b53006c72d7db7
Gabriel Filion <gabster@lelutin.ca>
2014-03-11 23:33:58 -0400

    Split modes and reuse resources
    client::config is easier to figure out this way, and we can reuse
    resources to remove unwanted parts.
    
    we're actually doing a better cleanup now.
    
    Signed-off-by: Gabriel Filion <gabster@lelutin.ca>

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -1,38 +1,16 @@
 class apt_cacher_ng::client::config {
 
-  File {
-    ensure => present,
-    owner  => root,
-    group  => 0,
-    mode   => '0644',
-  }
-
   if ! $apt_cacher_ng::client::autodetect {
-    $server = $apt_cacher_ng::client::servers[0]
-    file { '/etc/apt/apt.conf.d/71proxy':
-      content => "Acquire::http { Proxy \"http://${server}\"; };",
-    }
-    file { '/etc/apt/apt.conf.d/30detectproxy':
+    include apt_cacher_ng::client::one_proxy
+    class { 'apt_cacher_ng::client::autodetect':
       ensure => absent,
     }
   }
   else {
-    file { '/etc/apt/apt.conf.d/71proxy':
+    class { 'apt_cacher_ng::client::one_proxy':
       ensure => absent,
     }
-    file { '/etc/apt/apt.conf.d/30detectproxy':
-      source => 'puppet:///modules/apt_cacher_ng/30detectproxy',
-    }
-
-    $show_proxy_messages = bool2num($apt_cacher_ng::client::verbose)
-
-    file { '/etc/apt/detect-http-proxy':
-      content => template('apt_cacher_ng/detect-http-proxy.erb'),
-      mode    => '0755',
-    }
-    file { '/etc/apt/proxy.conf':
-      content => template('apt_cacher_ng/apt-proxy-conf.erb'),
-    }
+    include apt_cacher_ng::client::autodetect
   }
 
 }
